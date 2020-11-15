f960db055a6ce4b3a6be01c3b5d59dbfba072bd6
Cosmicscience <117581056+Cosmicscience@users.noreply.github.com>
2020-11-15 06:12:22 +0000

    1.8.1

---
+++ Coins_Counter/Form1.Designer.cs
@@ -0,0 +1,11 @@
+
+﻿namespace Coins_Counter
+{
+    partial class Coins_counter
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
