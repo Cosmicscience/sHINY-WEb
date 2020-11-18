b0e13eb4ad717c53f8d79d8e870e3f390e6adf02
Cosmicscience <117581056+Cosmicscience@users.noreply.github.com>
2020-11-18 22:45:58 +0000

    Release 0.0.21

--- Coins_Counter/Form1.cs
+++ Coins_Counter/Form1.cs
@@ -9,4 +9,5 @@ namespace Coins_Counter
         public Coins_counter()
         {
             InitializeComponent();
-            textBox1.AppendText("200");                         // Default sum (can
+            textBox1.AppendText("200");                         // Default sum (can be changed in main form)
+            textBox2.AppendText("200 100 50 20 10 5 2 1");      // Defaul
