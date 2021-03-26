60384a559891811fe5c7cc2d474797b8fc116494
Cosmicscience <117581056+Cosmicscience@users.noreply.github.com>
2021-03-26 21:31:13 +0000

    LOGO

--- Coins_Counter/Form1.cs
+++ Coins_Counter/Form1.cs
@@ -136,4 +136,7 @@ namespace Coins_Counter
         }
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if ((!char.IsDigit(e.KeyChar) == true)&&(!char.IsWhiteSpace(e.KeyChar) == t
+            if ((!char.IsDigit(e.KeyChar) == true)&&(!char.IsWhiteSpace(e.KeyChar) == true)&&(e.KeyChar != (char)8))
+            {
+                e.Handled = true;
+            }
