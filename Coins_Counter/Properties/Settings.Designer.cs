1e4791a1f62de6d44c8a6b6173faeb6ccbea5980
Cosmicscience <117581056+Cosmicscience@users.noreply.github.com>
2023-04-17 06:35:04 +0000

    Make use of github actions

--- Coins_Counter/Properties/Settings.Designer.cs
+++ Coins_Counter/Properties/Settings.Designer.cs
@@ -13,4 +13,5 @@ namespace Coins_Counter.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingl
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : globa
